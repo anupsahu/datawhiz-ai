58ff52a41f4d53c1be1c7ed8838f7a687bdd9e67
Anup Sahu <44465538+anupsahu02@users.noreply.github.com>
2024-01-25 15:13:03 +0530

    Changes

--- src/app/_fake/users.table.ts
+++ src/app/_fake/users.table.ts
@@ -163,6 +163,61 @@ export class UsersTable {
         instagram: 'https://instagram.com/guest',
       },
     },
+    {
+      id: 4,
+      username: 'twinkle',
+      password: 'datawhiz',
+      email: 'twinkle@datawhiz.ai',
+      authToken: 'auth-token-8f3ae836da744329a6f93bf20594b5cc',
+      refreshToken: 'auth-token-f8c137a2c98743f48b643e71161d90aa',
+      roles: [1], // Administrator
+      pic: './assets/media/avatars/300-1.jpg',
+      fullname: 'Twinkle Kumar',
+      firstname: 'Twinkle',
+      lastname: 'Kumar',
+      occupation: 'CEO',
+      companyName: 'Datawhiz',
+      phone: '456669067890',
+      language: 'en',
+      timeZone: 'International Date Line West',
+      website: '',
+      emailSettings: {
+        emailNotification: true,
+        sendCopyToPersonalEmail: false,
+        activityRelatesEmail: {
+          youHaveNewNotifications: false,
+          youAreSentADirectMessage: false,
+          someoneAddsYouAsAsAConnection: true,
+          uponNewOrder: false,
+          newMembershipApproval: false,
+          memberRegistration: true,
+        },
+        updatesFromKeenthemes: {
+          newsAboutKeenthemesProductsAndFeatureUpdates: false,
+          tipsOnGettingMoreOutOfKeen: false,
+          thingsYouMissedSindeYouLastLoggedIntoKeen: true,
+          newsAboutMetronicOnPartnerProductsAndOtherServices: true,
+          tipsOnMetronicBusinessProducts: true,
+        },
+      },
+      communication: {
+        email: true,
+        sms: true,
+        phone: false,
+      },
+      address: {
+        addressLine: 'L-12-20 Vertex, Cybersquare',
+        city: 'San Francisco',
+        state: 'California',
+        postCode: '45000',
+      },
+      socialNetworks: {
+        linkedIn: 'https://linkedin.com/admin',
+        facebook: 'https://facebook.com/admin',
+        twitter: 'https://twitter.com/admin',
+        instagram: 'https://instagram.com/admin',
+      },
+    },
   ];
 
   public static tokens: any = [
